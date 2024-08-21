a81c261b883992daa8225ce982c2f13e1c525f9b
LG33 <louisgaillard33@gmail.com>
2024-08-21 21:00:34 +0000

    added spoof patch

---
+++ src/main/kotlin/grindr/patches/grindr/spoof/SpoofAndroidDeviceIdPatch.kt
@@ -0,0 +1,50 @@
+package app.revanced.patches.grindr.spoof
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
+import app.revanced.patches.grindr.spoof.fingerprints.GetAndroidIDFingerprint
+import app.revanced.util.exception
+
+@Patch(
+    name = "Spoof Android device ID",
+    description = "Spoofs the Android device ID used by the app for account authentication. " +
+        "This can be used to copy the account to another device.",
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.grindrapp.android",
+            [
+                "24.9.0",
+                "24.10.0",
+                "24.11.0",
+                "24.12.0",
+                "24.13.0",
+            ],
+        ),
+    ],
+    use = false,
+)
+@Suppress("unused")
+object SpoofAndroidDeviceIdPatch : BytecodePatch(
+    setOf(GetAndroidIDFingerprint),
+) {
+    private var androidDeviceId =
+        stringPatchOption(
+            key = "android-device-id",
+            default = "0011223344556677",
+            title = "Android device ID",
+            description = "The Android device ID to spoof to.",
+            required = true,
+        ) { it!!.matches("[A-Fa-f0-9]{16}".toRegex()) }
+
+    override fun execute(context: BytecodeContext) = GetAndroidIDFingerprint.result?.mutableMethod?.addInstructions(
+        0,
+        """
+            const-string v0, "$androidDeviceId"
+            return-object v0
+        """,
+    ) ?: throw GetAndroidIDFingerprint.exception
+}
